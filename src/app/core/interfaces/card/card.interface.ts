0bf13ca19fec4c04cd8dc1f5627b4c6958ef565c
Henrique Malvar <henriquemalvar@gmail.com>
2024-02-15 23:42:31 -0300

    feat: update interfaces for cards

--- src/app/core/interfaces/card/card.interface.ts
+++ src/app/core/interfaces/card/card.interface.ts
@@ -1,12 +1,13 @@
-import { Category } from "@interfaces/category/category.interface";
+import { Category } from '@interfaces/category/category.interface';
 
 export interface Card {
   id: string;
-  status: string | "Não iniciado" | "Em progresso" | "Completo";
+  status: string | 'Não iniciado' | 'Em progresso' | 'Completo';
   title: string;
   description: string;
   createdAt: Date;
   updatedAt: Date;
   user_id: string;
   categories: Category[];
+  category_ids?: string[];
 }
